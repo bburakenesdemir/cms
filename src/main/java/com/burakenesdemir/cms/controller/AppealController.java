8c9710305e2baab255de432bd4b2d92174b4946d
Burak Enes Demir <enes.demir@kgteknoloji.com>
2020-03-30 16:55:12 +0300

    created Appeal Service and Appeal Controller

--- src/main/java/com/burakenesdemir/cms/controller/AppealController.java
+++ src/main/java/com/burakenesdemir/cms/controller/AppealController.java
@@ -1,4 +1,48 @@
 package com.burakenesdemir.cms.controller;
 
+import com.burakenesdemir.cms.data.entity.Appeal;
+import com.burakenesdemir.cms.service.AppealService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/appeal")
 public class AppealController {
+
+    @Autowired
+    AppealService appealService;
+
+    @PostMapping
+    public ResponseEntity<Appeal> create(@RequestBody Appeal appeal) {
+        appealService.create(appeal);
+
+        return new ResponseEntity<Appeal>(appeal, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Appeal> update(@RequestBody Appeal appeal) {
+        appealService.update(appeal);
+
+        return new ResponseEntity<Appeal>(appeal, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Appeal> delete(@RequestBody Appeal appeal) {
+        appealService.delete(appeal.getIdentifier());
+
+        return new ResponseEntity<Appeal>(appeal, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Appeal>> getAll() {
+        return ResponseEntity.ok().body(appealService.getAll());
+
+    }
+
+
 }
